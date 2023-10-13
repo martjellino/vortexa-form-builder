3c057154fea5ab46e9d07414a2e145801631fe42
Ibrahim Aji <ibrahim.aji@live.com>
2023-10-13 14:24:17 +0700

    add login ui

--- src/components/auth/components/auth.register.jsx
+++ src/components/auth/components/auth.register.jsx
@@ -6,26 +6,38 @@ import { EyeFilledIcon } from "./EyeFilledIcon";
 import { EyeSlashFilledIcon } from "./EyeSlashFilledIcon";
 import Link from "next/link";
 
-export const AuthLogin = () => {
+export const AuthRegister = () => {
   const [isVisible, setIsVisible] = useState(false);
   const toggleVisibility = () => setIsVisible(!isVisible);
   return (
-    <div className="flex flex-col items-center w-full h-full">
-      <div className="bg-white rounded-md  h-full lg:w-4/5 flex flex-col justify-center px-16 pt-16">
-        <h1 className="font-bold text-xl">Sign In</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <div className="bg-white rounded-md h-auto lg:w-4/5 flex flex-col justify-center px-16 pt-16">
+        <h1 className="font-bold text-xl">Sign Up</h1>
         <p className="font-light text-sm text-gray-400">
-          Type your detail and start crafting!
+          Welcome, join with us and get better data with forms and surveys.
         </p>
-        <form className="space-y-4 mt-8">
+        <form className="space-y-4 mt-4">
           <div className="space-y-2">
             <label htmlFor="username" className="text-sm">
-              Username or Email
+              Username
             </label>
             <Input
               isClearable
               size="md"
               type="text"
-              placeholder="johndoe@example.com"
+              placeholder="John Doe"
+              isRequired
+            />
+          </div>
+          <div className="space-y-2">
+            <label htmlFor="username" className="text-sm">
+              Email
+            </label>
+            <Input
+              isClearable
+              size="md"
+              type="text"
+              placeholder="johndoe@sample.com"
               isRequired
             />
           </div>
@@ -53,21 +65,37 @@ export const AuthLogin = () => {
               }
             />
           </div>
-          <div className="flex justify-between">
-            <Checkbox>
-              <p className="text-sm">Remember Me</p>
-            </Checkbox>
-            <Link href="" className="text-sm underline">
-              Forgot Password
-            </Link>
+          <div className="space-y-2">
+            <label htmlFor="password" className="text-sm">
+              Confirm Password
+            </label>
+            <Input
+              size="md"
+              placeholder="******"
+              isRequired
+              type={isVisible ? "text" : "password"}
+              endContent={
+                <button
+                  className="focus:outline-none"
+                  type="button"
+                  onClick={toggleVisibility}
+                >
+                  {isVisible ? (
+                    <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                  ) : (
+                    <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                  )}
+                </button>
+              }
+            />
           </div>
           <Button color="primary" className="w-full" size="md">
-            Sign In
+            Sign Up
           </Button>
         </form>
         <div className="relative flex py-5 items-center">
           <div className="flex-grow border-t border-gray-300"></div>
-          <span className="flex-shrink mx-4 text-gray-300 text-sm">Or</span>
+          <span className="flex-shrink mx-4  text-sm">Or</span>
           <div className="flex-grow border-t border-gray-300"></div>
         </div>
         <Link
@@ -78,9 +106,9 @@ export const AuthLogin = () => {
           <p className="text-base">Continue with Google</p>
         </Link>
         <p className="text-sm flex justify-center p-6">
-          Don't have an account ?&nbsp;
+          Already have an account ?&nbsp;
           <Link href="" className="font-bold underline">
-            Register
+            Login
           </Link>
         </p>
       </div>
